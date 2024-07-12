a0ffe9a2c7feec6a9d7ee4d9564f6e22093bf11d
Nikhil Sawant <105492871+NIKHILSAWANT07@users.noreply.github.com>
2024-07-13 01:36:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,5 +84,5 @@ def process_terms():
         return jsonify({"success": False, "error": "No terms extracted"})
 
 
-#if __name__ == '__main__':
- #   app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
